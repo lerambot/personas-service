019a35e86ec2b9234fa4c2543f37693f3c80b0d9
Leandro <lerambot@gmail.com>
2025-06-20 16:51:46 +0200

    Primer commit del proyecto personas-service

---
+++ src/main/java/com/empresa/seguros/personas/PersonasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.seguros.personas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonasServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/seguros/personas_service/PersonasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.seguros.personas_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
