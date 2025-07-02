23d3643ac356b81ca6904b00dce378753c4dca1c
lerambot <lerambot@gmail.com>
2025-07-02 11:11:56 +0200

    chore: add OpenAPI config with API info

---
+++ src/main/java/com/empresa/personas/config/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package com.empresa.personas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Personas Service API")
+                .version("1.0")
+                .description("Documentación de la API REST del microservicio de personas")
+            );
+    }
+}
